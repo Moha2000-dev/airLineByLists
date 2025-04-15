69b6e4ef3e73fe0f892988b1a1c90742ef7ff059
MOHAMMED <TOM@20377@GMAIL.COM>
2025-04-15 13:57:00 +0400

    add the show menu

--- Program.cs
+++ Program.cs
@@ -25,6 +25,32 @@
             Console.WriteLine("|Welcome to the Airline Reservation System!|");
             Console.WriteLine("--------------------------------------------");
         }
+        public static int ShowMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("If you are a user press 1, if admin press 2:");
+                if (!int.TryParse(Console.ReadLine(), out int userType)) continue;
+
+                if (userType == 1)
+                {
+                    Console.WriteLine("1. Book Flight\n2. Cancel Flight\n3. View Flights\n5. Search Flights\n6. View Flight Details\n9. Exit");
+                }
+                else if (userType == 2)
+                {
+                    Console.WriteLine("1. Book Flight\n2. Cancel Flight\n3. View Flights\n4. Add Flight\n5. Search Flights\n6. View Flight Details\n7. Update Departure\n9. Exit");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid user type.");
+                    continue;
+                }
+
+                Console.Write("Enter your choice: ");
+                if (int.TryParse(Console.ReadLine(), out int choice)) return choice;
+            }
+        }
 
     }
 }
